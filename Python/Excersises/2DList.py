0f24f1380456a1571dabcde6d0c4da1760acccc9
AliAdnan2107 <alad0472@cloud.edu.pe.ca>
2023-10-16 10:39:27 -0300

    Create 2DList.py

---
+++ Python/Excersises/2DList.py
@@ -0,0 +1,16 @@
+#Created By Ali Adnan
+#CMP521A
+
+#Variables
+
+datalist=[
+
+    ["Ali","16"],
+    ["Mr MacDougald","Unknown"],
+    ["Undetermined Person","35"]
+]
+
+#Code
+
+print (datalist)
+
