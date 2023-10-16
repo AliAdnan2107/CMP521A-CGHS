affd5db54899f6082c684213588c2da841d7b82c
AliAdnan2107 <alad0472@cloud.edu.pe.ca>
2023-10-16 10:42:54 -0300

    Update 2DList.py

--- Python/Excersises/2DList.py
+++ Python/Excersises/2DList.py
@@ -9,8 +9,42 @@ datalist=[
     ["Mr MacDougald","Unknown"],
     ["Undetermined Person","35"]
 ]
+datalist2=[
+
+    ["Ali","16"],
+    ["Mr MacDougald","Unknown"],
+    ["Undetermined Person","35"]
+]
+datalist3=[
+
+    ["Ali","16"],
+    ["Mr MacDougald","Unknown"],
+    ["Undetermined Person","35"]
+]
+datalist4=[
+
+    ["Ali","16"],
+    ["Mr MacDougald","Unknown"],
+    ["Undetermined Person","35"]
+]
+datalist5=[
+
+    ["Ali","16"],
+    ["Mr MacDougald","Unknown"],
+    ["Undetermined Person","35"]
+]
+datalist6=[
+
+    ["Ali","16"],
+    ["Mr MacDougald","Unknown"],
+    ["Undetermined Person","35"]
+]
 
 #Code
 
 print (datalist)
-
+print (datalist2)
+print (datalist3)
+print (datalist4)
+print (datalist5)
+print (datalist6)
