00cb55a1ab10f3d54e40556fd23ee5d36e20c633
AliAdnan2107 <alad0472@cloud.edu.pe.ca>
2023-10-17 09:28:09 -0300

    Create fstring.py

---
+++ Python/Excersises/fstring.py
@@ -0,0 +1,14 @@
+#Created By Ali Adnan
+#CMP521A
+
+
+#Variables
+
+name=input("What is your name? \n")
+age=input("What's your age? \n")
+country=input("Where are you from? \n")
+
+
+#Code
+
+print (f"Hi there {name}, I hear you are {age} years old and you come from {country}!")
