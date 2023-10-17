132d9caa6d58f50a4399dfea1d19c1ee29d4fc1d
AliAdnan2107 <alad0472@cloud.edu.pe.ca>
2023-10-17 09:52:51 -0300

    Create 2DListInput.py

---
+++ Python/Excersises/2DListInput.py
@@ -0,0 +1,23 @@
+#Created By Ali Adnan
+#CMP521A
+
+
+#Variables
+
+list=[]
+import random
+i=0
+j=0
+loopnum=-1
+
+#Code
+
+numlistrow=int(input("How many rows would you like the list to be? \n"))
+numlistcolumn=int(input("How many columns would you like the list to be? \n"))
+
+for i in range(numlistrow):
+    list.append([random.randint])
+    for j in range (numlistcolumn):
+        list[0].append(random.randint)
+    print (str(i) * len(list))
+
