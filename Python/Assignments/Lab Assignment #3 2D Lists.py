86178bb9811c90cffd392b6b963de8a1dcb24b83
AliAdnan2107 <alad0472@cloud.edu.pe.ca>
2023-10-18 11:40:24 -0300

    Update Lab Assignment #3 2D Lists.py

--- Python/Assignments/Lab Assignment #3 2D Lists.py
+++ Python/Assignments/Lab Assignment #3 2D Lists.py
@@ -21,6 +21,9 @@ check=True
 choice=0
 rowappend=""
 columnappend=""
+rowinp=0
+colinp=0
+inc=0
 
 #Libraries
 
@@ -36,13 +39,35 @@ while check==True:
     print ("3- Print A Specific Data Point")
     print ("4- Print The Entire List")
     print ("5- Quit")
-    choice=int(input("What is your pick? : "))
-    if choice == 1:
-        rowappend=input("What would you like to add to your row of data? \n")
-        mainlist.append(rowappend)
-    elif choice ==2:
-        columnappend=input("What would you like to add to your column of data? \n")
-        
+    print ("")
+    choice=input("What is your pick? : ")
+    if choice.isdigit():
+        if int(choice) == 1:
+            rowappend=input("What would you like to add to your row of data? Make sure your data has a space and comma between information \n")
+            mainlist.append(rowappend)
+        elif int(choice) ==2:
+            for i in range (len(mainlist)):
+             columnappend=input("What would you like to add to your column of data? This will affect every row \n")
+             mainlist[i].append(columnappend)
+        elif int(choice) ==3:
+            rowinp=int(input("Please type in the row number : "))
+            colinp=int(input("Please type in the column number : "))
+            print ("")
+            print (mainlist[rowinp][colinp])
+            print ("")
+        elif int(choice) ==4:
+            print ("")
+            for x in range (len(mainlist)):
+                print (mainlist[x])
+            print ("")
+        elif int(choice) ==5:
+            print ("Thank you for using my program!")
+            break
+        else:
+            print ("Number from 1-5 Please.")
+    else:
+        print ("Just quit the program.")
+
 
 
 
