7cdf8e221d13d3039c628520ceb56a975b1a62dc
AliAdnan2107 <alad0472@cloud.edu.pe.ca>
2023-10-19 09:34:17 -0300

    Update Lab Assignment #3 2D Lists.py

--- Python/Assignments/Lab Assignment #3 2D Lists.py
+++ Python/Assignments/Lab Assignment #3 2D Lists.py
@@ -6,7 +6,7 @@
 
 #Variables
 
-mainlist=[
+mainlist=[ #The List That Holds All Information
     ["Name","Age","Job","Interest"],
     ["Agam","16","Unemployed","Soccer"],
     ["Ali","16","Bingo Worker","Computers"],
@@ -17,6 +17,7 @@ mainlist=[
     ["Jayden","16","Unemployed","Education"]
 ]
 
+splitlist=[]
 check=True
 choice=0
 rowappend=""
@@ -25,14 +26,10 @@ rowinp=0
 colinp=0
 inc=0
 
-#Libraries
-
-
-
 #Code
 
 print ("Welcome to my program!")
-while check==True:
+while check==True: #Main Loop of program
     print ("Please Pick an option")
     print ("1 - Add A Row Of Data")
     print ("2- Add A Column Of Data")
@@ -42,33 +39,31 @@ while check==True:
     print ("")
     choice=input("What is your pick? : ")
     if choice.isdigit():
-        if int(choice) == 1:
-            rowappend=input("What would you like to add to your row of data? Make sure your data has a space and comma between information \n")
+        if int(choice) == 1: #Row Of Data Code
+            splitlist=input("What would you like to add to your row of data? Make sure your data has a space and comma between information \n")
+            rowappend=splitlist.split(" , ")
             mainlist.append(rowappend)
-        elif int(choice) ==2:
+        elif int(choice) ==2: #Column Of Data Code
             for i in range (len(mainlist)):
              columnappend=input("What would you like to add to your column of data? This will affect every row \n")
              mainlist[i].append(columnappend)
-        elif int(choice) ==3:
+        elif int(choice) ==3: #Print A Specific Data Point
             rowinp=int(input("Please type in the row number : "))
             colinp=int(input("Please type in the column number : "))
             print ("")
             print (mainlist[rowinp][colinp])
             print ("")
-        elif int(choice) ==4:
+        elif int(choice) ==4: #Print The Entire List
             print ("")
             for x in range (len(mainlist)):
                 print (mainlist[x])
             print ("")
-        elif int(choice) ==5:
+        elif int(choice) ==5: #Quit
             print ("Thank you for using my program!")
             break
-        else:
+        else: #Safeguard for any number above or under 1-5
             print ("Number from 1-5 Please.")
-    else:
-        print ("Just quit the program.")
-
-
-
-
-
+            print ("")
+    else: #Safeguard for anyone who inputs a string instead of an integer
+        print ("Numbers only.")
+        print ("")
