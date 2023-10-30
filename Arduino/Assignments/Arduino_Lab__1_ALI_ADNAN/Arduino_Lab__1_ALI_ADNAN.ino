831db3abf123206b4ad14467f8bf31fc55aaa0a3
AliAdnan2107 <alad0472@cloud.edu.pe.ca>
2023-10-30 11:34:42 -0300

    Arduino Scraps

--- Arduino/Assignments/Arduino_Lab__1_ALI_ADNAN/Arduino_Lab__1_ALI_ADNAN.ino
+++ Arduino/Assignments/Arduino_Lab__1_ALI_ADNAN/Arduino_Lab__1_ALI_ADNAN.ino
@@ -6,18 +6,15 @@
 
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(5, OUTPUT);
   pinMode(10, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   digitalWrite(5, HIGH); //Turn On (Blue Light)
-  delay(2000);                    // Stay On (Red Light)
+  delay(2000);                    // Stay On (Blue Light)
   digitalWrite(5, LOW);  // Turn Off (Blue Light)
   digitalWrite(10, HIGH); //Turn On (Red Light)
   delay(2000);                    // Stay On (Red Light)
   digitalWrite(10, LOW); // Turn Off (Red Light)
-
 }
