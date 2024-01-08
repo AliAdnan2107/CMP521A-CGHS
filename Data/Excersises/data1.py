69d536f50d24900b12591de92049325ffced03aa
AliAdnan2107 <alad0472@cloud.edu.pe.ca>
2024-01-08 11:11:31 -0400

    Update data1.py

--- Data/Excersises/data1.py
+++ Data/Excersises/data1.py
@@ -1,5 +1,26 @@
 import matplotlib.pyplot as plt 
 
-plt.plot([2,4,6],[4,5,7])
-
+x=[1,2,3]
+y=[3,2,1]
+x2=[2,4,6]
+y2=[6,4,2]
+plt.scatter(x,y, label="This bar means something.", color="r", s=100, marker="D")
+plt.scatter(x2,y2, Label="This bar also means something", color="b", s=100, marker="D")
+plt.xlabel("idk what to type here")
+plt.ylabel("send help!")
+plt.title ("i love comp sci (optional)")
+plt.legend()
 plt.show()
+'''
+populationages=[22,44,11,13,15,17,21,42,85,93,13,53,16]
+
+ids = [x for x in range (len(populationages))]
+
+plt.bar(ids, populationages, color="rgb", label="Population")
+
+plt.xlabel ("Average")
+plt.ylabel ("Ages")
+plt.title ("Average Population Age")
+plt.legend()
+plt.show()
+'''
