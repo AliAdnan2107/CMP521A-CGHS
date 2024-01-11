9e917ea0e299ddc804dd448006056cbca240db15
AliAdnan2107 <alad0472@cloud.edu.pe.ca>
2024-01-11 10:07:42 -0400

    more... data....

---
+++ Data/Excersises/collision/collision.py
@@ -0,0 +1,35 @@
+#Variables
+import matplotlib.pyplot as plt 
+import csv
+
+#Code
+
+data=[]
+x=[]
+y=[]
+a=0
+i=0
+path="Collisions_from_1980.csv"
+file=open(path,newline="")
+reader=csv.reader(file)
+
+header=next(reader)
+data=[row for row in reader]
+
+
+for a in range (len(data)):
+    col1=data[a][0]
+    col2=data[a][1]
+    x.append(int(col1))
+    y.append(int(col2))
+
+
+for i in range (len(x)):
+    print (x[i],y[i])
+
+plt.plot(x,y)
+plt.xlabel("Year")
+plt.ylabel("Number of Injured")
+plt.title("Number Of Injured Vs Year")
+plt.legend()
+plt.show()
