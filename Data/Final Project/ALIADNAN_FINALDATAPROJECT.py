f2af1071f1e36a8b4a88126ffc00a5cb42d48135
AliAdnan2107 <alad0472@cloud.edu.pe.ca>
2024-01-24 11:09:33 -0400

    Final Commit.

--- Data/Final Project/ALIADNAN_FINALDATAPROJECT.py
+++ Data/Final Project/ALIADNAN_FINALDATAPROJECT.py
@@ -9,12 +9,12 @@ TODO-
 COMPLETE!
 '''
 
-#Variables & Libaries
-
+#Libraries
 import matplotlib.pyplot as plt
 import locale
 import csv
 import random
+#Variables
 header1=["Track Name","Artist Name","Artist Count","Released Year","Released Month","Released Day"]
 sorted_track_names = []
 sorted_artist_names = []
@@ -50,26 +50,26 @@ while True:
         print (" 2 - By Artist Name")
         print (" 3 - By Released Year")
         indexinp=int(input("Please pick a indexing option : "))
-        if indexinp is 1:
+        if indexinp is 1: #Track Name 
             track_name = input("Please enter the name of your Track (SOME CHARACTERS ARENT RECOGNIZED) : ").lower()
             print("")
-            print (', '.join(header1))
+            print (', '.join(header1)) #Print Header
             for row in data:
-                if row[0].lower() == track_name:
+                if row[0].lower() == track_name: #If data is found, print data.
                     print(', '.join(row[:6]))
-        elif indexinp is 2:
+        elif indexinp is 2: #Artist Name
             artist_name = input("Please enter the name of the Artist (SOME CHARACTERS ARENT RECOGNIZED) : ").lower()
             print("")
-            print (', '.join(header1))
+            print (', '.join(header1)) #Print Header
             for row in data:
-                if artist_name in row[1].lower():
+                if artist_name in row[1].lower(): #If data is found, print data.
                     print(', '.join(row[:6]))
-        elif indexinp is 3:
+        elif indexinp is 3: #Released Year
             released_year = int(input("Please enter the released year: "))
             print("")
-            print (', '.join(header1))
+            print (', '.join(header1)) #Print Header
             for row in data:
-                if int(row[3]) == released_year:
+                if int(row[3]) == released_year: #if Year is found, print data.
                     print(', '.join(row[:6]))
         else:
             print("")
@@ -82,49 +82,49 @@ while True:
         print (" 2 - Artist Name")
         print (" 3 - Released Year")
         sortinp=int(input("Please pick a sorting option : "))
-        if sortinp == 1:
-            sortinp2=int(input("Alphabetical Order (1) or Reversed Alphabetical Order (2) : "))
-            if sortinp2 is 1:
-                sorted_data = sorted(data[1:], key=lambda x: x[0], reverse=False)  # Sort by Track Name
+        if sortinp == 1: #Track Name
+            sortinp2=int(input("Alphabetical Order (1) or Reversed Alphabetical Order (2) : ")) #Pick order
+            if sortinp2 is 1: #If Alphabetical
+                sorted_data = sorted(data[1:], key=lambda x: x[0], reverse=False)  # Sort Track Name
                 for row in sorted_data:
                     print (row)
                     sorted_track_names.append(row[0])
                     print ("")
                 print ("All data sorted into a list.")
-            elif sortinp2 is 2: 
-                sorted_data = sorted(data[1:], key=lambda x: x[0], reverse=True)  # Sort by Track Name
+            elif sortinp2 is 2:  #If Reversed Alphabetical
+                sorted_data = sorted(data[1:], key=lambda x: x[0], reverse=True)  # Sort Track Name Reversed
                 for row in sorted_data:
                     print (row)
                     sorted_track_names.append(row[0])
                     print ("")
                 print ("All data sorted into a list.")
-        elif sortinp == 2:
+        elif sortinp == 2: #Artist Name
             sortinp2=int(input("Alphabetical Order (1) or Reversed Alphabetical Order (2) : "))
-            if sortinp2 is 1:
-                sorted_data = sorted(data[1:], key=lambda x: x[1], reverse=False)  # Sort by Track Name
+            if sortinp2 is 1: #If Alphabetical
+                sorted_data = sorted(data[1:], key=lambda x: x[1], reverse=False)  # Sort Track Name
                 for row in sorted_data:
                     print (row)
                     sorted_artist_names.append(row[0])
                     print ("")
                 print ("All data sorted into a list.")
-            elif sortinp2 is 2: 
-                sorted_data = sorted(data[1:], key=lambda x: x[1], reverse=True)  # Sort by Track Name
+            elif sortinp2 is 2: #If Reversed Alphabetical
+                sorted_data = sorted(data[1:], key=lambda x: x[1], reverse=True)  # Sort Track Name Reversed
                 for row in sorted_data:
                     print (row)
                     sorted_artist_names.append(row[0])
                     print ("")
                 print ("All data sorted into a list.")
-        elif sortinp == 3:
+        elif sortinp == 3: #Released Year
             sortinp2=int(input("Least to Greatest (1) or Greatest to least? (2) : "))
-            if sortinp2 is 1:
-                sorted_data = sorted(data[1:], key=lambda x: (int(x[3]), int(x[4])), reverse=True)  # Sort by Released Year
+            if sortinp2 is 1: #If Least To Greatest
+                sorted_data = sorted(data[1:], key=lambda x: (int(x[3]), int(x[4])), reverse=True)  # Sort Released Year
                 for row in sorted_data:
                     print(row)
                     sorted_released_years.append(row[3])
                     print ("")
                 print ("All data sorted into a list.")
-            elif sortinp2 is 2:
-                sorted_data = sorted(data[1:], key=lambda x: (int(x[3]), int(x[4])), reverse=False)  # Sort by Released Year
+            elif sortinp2 is 2: #If Greatest To Least
+                sorted_data = sorted(data[1:], key=lambda x: (int(x[3]), int(x[4])), reverse=False)  # Sort Released Year Reversed
                 for row in sorted_data:
                     print(row)
                     sorted_released_years.append(row[3])
@@ -141,26 +141,26 @@ while True:
         print(" 2 - 10 Random Artists vs Highest Streamed Song")
         print(" 3 - Released Year vs Artist Count")
         graphinp = int(input("Please pick a graphing option : "))  
-        if graphinp == 1:
+        if graphinp == 1: #10 Random songs Vs Released Year
             num_tracks = len(sorted_track_names)
-            random_indices = random.sample(range(num_tracks), 10)
+            random_indices = random.sample(range(num_tracks), 10) #Pick 10 Random
             plt.figure(figsize=(10, 6))
             for i in random_indices:
                 x_value = i + 1
                 y_value = sorted_released_years[i]
-                plt.scatter(x_value, y_value, label=sorted_track_names[i])
+                plt.scatter(x_value, y_value, label=sorted_track_names[i]) #Scatter over plot
             plt.xlabel('Track')
             plt.ylabel('Released Year')
             plt.title('Randomly Selected 10 Tracks vs Released Year')
             plt.legend()
             plt.show()
-        elif graphinp == 2:
-            random_artists = random.sample(sorted_data, 10)
-            max_streams = max(int(row[8]) for row in sorted_data[:10])
+        elif graphinp == 2: #10 Random Artists vs Highest Streamed Songs
+            random_artists = random.sample(sorted_data, 10) #10 Random
+            max_streams = max(int(row[8]) for row in sorted_data[:10]) #Find Highest Streams
             step_size = max_streams // 10 if max_streams % 10 == 0 else max_streams // 10 + 1
             y_ticks = [locale.format_string("%d", i, grouping=True) for i in range(0, max_streams + 1, step_size)]
             plt.figure(figsize=(10, 6))
-            plt.bar([row[1] for row in random_artists], [int(row[8]) for row in random_artists])  # Assuming Streams is in column [8]
+            plt.bar([row[1] for row in random_artists], [int(row[8]) for row in random_artists])
             plt.xlabel('Artist Name')
             plt.ylabel('Streams')
             plt.title('10 Random Artists vs Highest Streamed Song')
@@ -168,9 +168,9 @@ while True:
             locale.setlocale(locale.LC_ALL, '')
             plt.yticks(range(0, max_streams + 1, step_size), y_ticks)  # Adjust the range and step based on your data
             plt.show()
-        elif graphinp is 3:
+        elif graphinp is 3: #Released Year vs Artist Count
             plt.figure(figsize=(10, 6))
-            plt.plot([int(row[3]) for row in sorted_data], [int(row[2]) for row in sorted_data])  # Assuming Released Year is in column [3] and Artist Count is in column [2]
+            plt.plot([int(row[3]) for row in sorted_data], [int(row[2]) for row in sorted_data]) #Plot sorted data
             plt.xlabel('Released Year')
             plt.ylabel('Artist Count')
             plt.title('Released Year vs Artist Count')
